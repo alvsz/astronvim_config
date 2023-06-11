e8e0b633a764c424d3be055296fd238e4b84618c
João Gabriel Alves <joao.aac@disroot.org>
2023-06-11 13:24:33 -0300

    plugins e treesitter

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,22 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = false
+            })
+        end
+    }, {'elkowar/yuck.vim', lazy = false}, {'ron-rs/ron.vim', lazy = false}
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
 }
